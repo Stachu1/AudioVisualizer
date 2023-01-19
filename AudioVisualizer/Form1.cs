b118818bbbf027df71851b4d872f6a76c2ba04d2
Stachu1 <stach.basiuk@gmail.com>
2023-01-19 20:49:38 +0100

    Added: automatic alignment to the Spotify window (only when nothing is playing), wave image in the top left corrner

--- AudioVisualizer/Form1.cs
+++ AudioVisualizer/Form1.cs
@@ -8,15 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ezOverLay;
+using NAudio;
 
 namespace AudioVisualizer
 {
     public partial class Form1 : Form
     {
         ez ez = new ez();
+        Int16[] dataPcm;
+
+        Pen pen1 = new Pen(Color.White);
+        Pen pen2 = new Pen(Color.LightGreen);
+        Brush brush1 = new SolidBrush(Color.White);
+        Brush brush2 = new SolidBrush(Color.LightGreen);
         public Form1()
         {
             InitializeComponent();
+            AudioMonitorInitialize(0);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -31,7 +39,66 @@ namespace AudioVisualizer
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            g.DrawRectangle(new Pen(Color.Red), 0, 0, pictureBox1.Width-1, pictureBox1.Height-1);
+            g.DrawRectangle(pen2, 0, 0, pictureBox1.Width-1, pictureBox1.Height-1);
+            DrawWav(g);
+        }
+
+        private void DrawWav(Graphics g, int x = 0, int y = 3, int width = 251, int height = 50)
+        {
+            g.FillRectangle(new SolidBrush(Color.Black), 20, 15, 30, 30);
+            if (dataPcm != null)
+            {
+                int max_value = dataPcm.Max();
+                int delta = dataPcm.Length / width;
+                if (max_value > 15)
+                {
+                    for (int i = dataPcm.Length; i >= 0; i--)
+                    {
+                        if (i % delta == 0 && i / delta <= width)
+                        {
+                            g.FillRectangle(brush2, x + i / delta, y + height / 2 + (dataPcm[i] * height) / (2 * max_value), 1, 1);
+                        }
+                    }
+                }
+                else
+                {
+                    g.DrawLine(pen2, x, y + height / 2, x + width, y + height / 2);
+                }
+            }
+        }
+
+
+        private NAudio.Wave.WaveInEvent wvin;
+        private void AudioMonitorInitialize(
+                int DeviceIndex, int sampleRate = 32_000,
+                int bitRate = 16, int channels = 1,
+                int bufferMilliseconds = 30, bool start = true)
+        {
+            if (wvin == null)
+            {
+                wvin = new NAudio.Wave.WaveInEvent();
+                wvin.DeviceNumber = DeviceIndex;
+                wvin.WaveFormat = new NAudio.Wave.WaveFormat(sampleRate, bitRate, channels);
+                wvin.DataAvailable += OnDataAvailable;
+                wvin.BufferMilliseconds = bufferMilliseconds;
+                if (start)
+                    wvin.StartRecording();
+            }
+        }
+
+        private void OnDataAvailable(object sender, NAudio.Wave.WaveInEventArgs args)
+        {
+            int bytesPerSample = wvin.WaveFormat.BitsPerSample / 8;
+            int samplesRecorded = args.BytesRecorded / bytesPerSample;
+            if (dataPcm == null)
+                dataPcm = new Int16[samplesRecorded];
+            for (int i = 0; i < samplesRecorded; i++)
+                dataPcm[i] = BitConverter.ToInt16(args.Buffer, i * bytesPerSample);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
         }
     }
 }
